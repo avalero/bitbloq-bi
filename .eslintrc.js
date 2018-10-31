e85022b88171e36f0b5b9fb7dc997871c82d070a
Alberto Valero <alberto.valero@bq.com>
2018-10-31 19:08:41 +0100

    connecting with DB

---
+++ .eslintrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "parser": "typescript-eslint-parser"
+};
---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import {MongoClient, Collection} from 'mongodb';
+
+const url:string = 'mongodb://localhost:27017';
+const dbName = 'bitbloq';
+
+const countUsers = async (collection:Collection):Promise<number> =>{
+  const items = await collection.find().toArray();
+  return items.length;
+}
+
+const countChromeAppUsers = async (collection:Collection, chromeapp:boolean = true):Promise<number> =>{
+  const items = await collection.find({chromeapp}).toArray();
+  return items.length;
+}
+
+
+(async () => {
+  
+  try{
+    const client = await MongoClient.connect(url,{useNewUrlParser: true});
+    const db = await client.db(dbName);
+    console.log('Connected to db'); 
+    const collection = await db.collection('users');
+    console.log('Colletion opened');
+    let number:number = await countUsers(collection);
+    console.log(`Hay ${number} usuarios`);
+    number = await countChromeAppUsers(collection, false);
+    console.log(`Hay ${number} usuarios con chromeApp`);
+    if(client) client.close();
+  }catch(error){
+    console.log(error);
+  }
+})();
+
+
